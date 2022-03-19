30f86a4c16f9d255fe7ade7e7a7a6827341795ad
DarrelA <darrelaiscoding@gmail.com>
2022-03-19 22:45:31 +0800

    Added Leaflet map to Listing page.

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,6 +6,7 @@ import { doc, getDoc } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import formatMoney from 'utils/formatMoney';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 const Listing = () => {
   const [listing, setListing] = useState([]);
@@ -83,7 +84,26 @@ const Listing = () => {
 
         <p className='listingLocationTitle'>Location</p>
 
-        {/* @TODO: MAP */}
+        <div className='leafletContainer'>
+          <MapContainer
+            style={{ height: '100%', width: '100%' }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+            />
+
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
             to={`/contact/${listing.userRef}?listingName=${listing.name}`}
